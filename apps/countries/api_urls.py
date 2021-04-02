03ef115adf66edd1e4dc3c3f1777a7f489ff7559
SSCastanedaC <sscastanedac@gmail.com>
2021-04-02 13:22:11 -0500

    feat Eliminar restriccion login_required a API get_countries

--- apps/countries/api_urls.py
+++ apps/countries/api_urls.py
@@ -3,5 +3,5 @@ from django.contrib.auth.decorators import login_required
 from apps.countries.api import get_countries
 
 urlpatterns = [
-    path('countries', login_required(get_countries), name='countries')
+    path('countries', get_countries, name='countries')
 ]
