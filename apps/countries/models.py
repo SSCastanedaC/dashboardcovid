97df67124a42d44ce61eb8ff187639a3ac5b2365
SSCastanedaC <sscastanedac@gmail.com>
2021-04-02 02:09:48 -0500

    feat Crear modelo Country

--- apps/countries/models.py
+++ apps/countries/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Country(models.Model):
-    name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100)
-    code = models.CharField(max_length=5)
+    name_spanish = models.CharField(max_length=100)
+    name_english = models.CharField(max_length=100)
+    slug_spanish = models.SlugField(max_length=100)
+    slug_english = models.SlugField(max_length=100)
+    code_alpha_two = models.CharField(max_length=5)
+    code_alpha_three = models.CharField(max_length=5)
