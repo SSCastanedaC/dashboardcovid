d920a7212724915e5a73831563ad9b58642bdc02
SSCastanedaC <sscastanedac@gmail.com>
2021-04-01 21:19:46 -0500

    feat Configurar Logout + Master Page

--- dashboardcovid/settings.py
+++ dashboardcovid/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'apps.cases',
     'apps.countries',
 ]
@@ -127,3 +128,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+
+headers_rapidapi = {
+    'x-rapidapi-key': "dc94ab56dcmshfa9f4aa92abbe8fp172759jsne47737395746",
+    'x-rapidapi-host': "covid-19-data.p.rapidapi.com"
+}
--- dashboardcovid/urls.py
+++ dashboardcovid/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
@@ -20,5 +22,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='web/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
     path('cases/', include(('apps.cases.urls', 'cases'), namespace='cases'))
 ]
