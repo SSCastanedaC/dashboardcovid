cdf7dc7845432fa7ce20d8a06419ff1216b14062
SSCastanedaC <sscastanedac@gmail.com>
2021-04-01 21:18:50 -0500

    feat Consumir API Casos globales

--- apps/cases/views.py
+++ apps/cases/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from apps.cases.models import Case
+from dashboardcovid.settings import headers_rapidapi
+import requests
+import datetime
+import json
 
 # Create your views here.
 
 def get_home(request):
-    context = {}
-    return render(request, 'web/home.html' ,context)
+    url = 'https://covid-19-data.p.rapidapi.com/report/totals'
+    response = requests.request('GET', url, headers=headers_rapidapi)
+    cases = response.json()
+    context = {
+        'cases': cases,
+    }
+    return render (request, 'web/home.html' ,context)
